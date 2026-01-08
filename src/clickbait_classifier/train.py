0f2effbdc3779eda6048d9d02939e420c7653f8b
poulguoskov <poul.g.skov@gmail.com>
2026-01-08 18:25:42 +0100

    Add cookiecutter project structure

---
+++ src/clickbait_classifier/train.py
@@ -0,0 +1,10 @@
+from clickbait_classifier.model import Model
+from clickbait_classifier.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
