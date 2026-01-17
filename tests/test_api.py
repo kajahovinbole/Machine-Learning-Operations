93814c4c4e44577da2766d145c3202d79d4408bd
melinajonesb <s253737@dtu.dk>
2026-01-17 14:36:34 +0100

    fixing ruff

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,21 +1,22 @@
+from unittest.mock import patch
+
 from fastapi.testclient import TestClient
 
 from src.clickbait_classifier.api import app
 
 client = TestClient(app)
 
+
 def test_read_main():
-    """Sjekker at root-endepunktet svarer."""
     response = client.get("/")
     assert response.status_code == 200
-    # Tilpass denne meldingen til det API-et ditt faktisk returnerer
     assert response.json() == {"message": "OK", "status-code": 200}
 
 
-def test_predict_endpoint():
+@patch("src.clickbait_classifier.api.tokenizer")
+@patch("src.clickbait_classifier.api.model")
+def test_predict_endpoint(mock_model, mock_tokenizer):
     with TestClient(app) as client:
         response = client.post("/predict?text=Dette er en test")
         assert response.status_code == 200
-        json_data = response.json()
-        assert "is_clickbait" in json_data
-        assert json_data["text"] == "Dette er en test"
+        assert "is_clickbait" in response.json()
